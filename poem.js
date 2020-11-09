8ad1b42137c3be50aa2c4fff9589610c6c026693
Marius Balčytis <marius.balcytis@gmail.com>
2020-11-09 23:55:58 +0200

    That dreadful poem

---
+++ poem.js
@@ -0,0 +1,37 @@
+// poem by Marius Balčytis
+// bit.ly/those-dreadful-viruses
+// Copy -> Ctrl+Shift+J / Cmd+Option+J -> Paste to Console -> Enter
+(
+  (when, Johnny, hates, that, stupid, virus) =>
+  virus(hates(that(stupid, Johnny)))()
+)([],
+  poor => (wretched, Johnny) => 
+  	[...poor(wretched), 'humans'],
+
+  (devastated, dreadful = 0) => hollow =>
+  spaces => ((humans = (faces) =>
+      devastated(hollow)(spaces)) =>
+
+    ((times = (when) => humans((spread) => {}), those = [], rumors = 0) => (
+      dreadful++ > 1 ? [] : [...times(), 'annoy', ...those, ...humans()]
+    ))())(),
+
+  (anticipating, future, together = 0) =>
+  separately => however =>
+  (
+    (gravely = separately, untogether = however) =>
+    (together++ & 1 ? anticipating : future)
+    (gravely)(untogether)
+  )(),
+
+  cant => stand => ((those = [], biases = 0) => 
+  [...cant(stand), ...those, 'viruses'])(),
+
+  poor => (Johnny => Johnny(Johnny))(
+    ((oh) => wretched => boy =>
+      ((you = 0, human = wretched, badly = 0) =>
+        poor(wretched(human))(boy)
+      )()
+    )()
+  ),
+)
